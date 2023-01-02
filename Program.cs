d4ec29b6855a9d1de7f3f3a443672caa92b27a5c
Vini5577 <ugopessoa@gmail.com>
2023-01-02 10:53:08 -0300

    finalizando o desafio 2

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Linq;
+
+class Program{
+
+    static void Main(string[] args)
+    {
+        int qt = int.Parse(System.Console.ReadLine());
+        string[] num = new string[2];
+// TODO: Crie as outras condições necessárias para a resolução do desafio:
+        for (int i = 0; i < qt; i++)
+        {
+            num = Console.ReadLine().Split(" ");
+
+            string a = num[0];
+            string b = num[1];
+
+            if (a.EndsWith(b))
+                System.Console.WriteLine("encaixa");
+            else
+                System.Console.WriteLine("nao encaixa");
+        }
+    }
+}
